fc547314b18ed7bc2a633319ee14ed6612eeaff6
Everett <xiaopengsvip@users.noreply.github.com>
2026-06-02 01:52:35 +0800

    fix(android): preload system OpenSSL via QLibrary (v0.1.2)

--- clients/qt/src/main.cpp
+++ clients/qt/src/main.cpp
@@ -5,6 +5,11 @@
 #include <QIcon>
 #include <QSettings>
 #include <QNetworkProxy>
+#include <QSslSocket>
+
+#ifdef Q_OS_ANDROID
+#include <QLibrary>
+#endif
 
 #include "apimanager.h"
 #include "authmanager.h"
@@ -16,9 +21,20 @@ int main(int argc, char *argv[])
     app.setOrganizationName("AIOS");
     app.setOrganizationDomain("aios.vios.top");
     app.setApplicationName("AI超级工作台");
-    app.setApplicationVersion("0.1.1");
+    app.setApplicationVersion("0.1.2");
 
     QQuickStyle::setStyle("Basic");
+
+#ifdef Q_OS_ANDROID
+    // Pre-load system OpenSSL libraries on Android
+    QLibrary sslLib("ssl");
+    sslLib.setLoadHints(QLibrary::ExportExternalSymbolsHint);
+    sslLib.load();
+
+    QLibrary cryptoLib("crypto");
+    cryptoLib.setLoadHints(QLibrary::ExportExternalSymbolsHint);
+    cryptoLib.load();
+#endif
 
     QQmlApplicationEngine engine;
 
